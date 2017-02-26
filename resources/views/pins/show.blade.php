a55c307d4452cb89806d7192e1bc28aba0ce55af
MakeleAndzelo <97.piotrowski.michal@gmail.com>
2017-02-26 21:05:46 +0100

    Added styling for show page

--- resources/views/pins/show.blade.php
+++ resources/views/pins/show.blade.php
@@ -1,27 +1,65 @@
 @extends('layouts.app')
 
 @section('content')
-	<h1>{{ $pin->name }}</h1>
-	<p>
-		{!!  $pin->description !!}
-	</p>
-	<img src="{{ '/' . $pin->image_path }} ">
+	<div class="columns">
+		<div class="column is-8 is-offset-2">
+			<div class="card">
+				<header class="card-header">
+				    <p class="card-header-title">
+				      {{ $pin->name }}
+				    </p>
+				</header>
+			  <div class="card-image">
+			    <figure class="image is-4by3">
+			      <img src="{{ '/' . $pin->image_path }} ">
+			    </figure>
+			  </div>
+			  <div class="card-content">
+			  	<div class="media-left">
+				@if(!$pin->liked())
+					<a href="{{ route('pins.upvote', $pin->id) }}" class="button">
+						<span class="icon is-small">
+							<i class="fa fa-thumbs-up" aria-hidden="true"></i>
+						</span>
+					</a>
+				@else
+					<a href="{{ route('pins.downvote', $pin->id) }}" class="button">
+						<span class="icon is-small">
+							<i class="fa fa-thumbs-down" aria-hidden="true"></i>
+						</span>
+					</a>
+				@endif
+				  <a id="delete-btn" class="button is-danger is-outlined">
+				    <span>Delete</span>
+				    <span class="icon is-small">
+				      <i class="fa fa-times"></i>
+				    </span>
+				  </a>
+				<form id="delete-form" action="{{ route('pins.destroy', $pin->id) }}" method="POST">
+					{{ csrf_field() }}
+					{{ method_field("DELETE") }}
+				</form>
+			  	</div>
+			    <div class="media">
+			      <div class="media-content">
+			        <p class="title is-4">{{ $pin->user->name }}</p>
+			        <p class="subtitle is-6">@ {{ $pin->user->email }}</p>
+			      </div>
+			    </div>
 
-	<small>{{ $pin->likeCount }}</small>
-	@if(!$pin->liked())
-		<a href="{{ route('pins.upvote', $pin->id) }}">Upvote</a>
-	@else
-		<a href="{{ route('pins.downvote', $pin->id) }}">Downvote</a>
-	@endif
-	<h1>
-		<form action="{{ route('pins.destroy', $pin->id) }}" method="POST">
-			{{ csrf_field() }}
-			{{ method_field("DELETE") }}
-			<button type="submit">Delete</button>
-		</form>
-	</h1>
-	<hr>
-	<div id="disqus_thread"></div>
+			    <div class="content">
+			      {!!  $pin->description !!}
+			      <br>
+			      <small>Posted {{ $pin->created_at->diffForHumans() }}</small>
+			      <br>
+			      <small>{{ $pin->likeCount }} {{ str_plural('like', $pin->likeCount) }}</small>
+			    </div>
+			  </div>
+			</div>
+			<hr>
+			<div id="disqus_thread"></div>
+		</div>
+	</div>
 @endsection
 
 @section('footer')
@@ -42,6 +80,7 @@
             s.setAttribute('data-timestamp', +new Date());
             (d.head || d.body).appendChild(s);
         })();
+
 	</script>
 	<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
 @endsection
